edff5b966655ccf89c71f353650c71c352e6c637
Petersen <apetersen@skylinetechnologies.com>
2020-03-23 00:31:04 -0500

    re-exported with the latest tech items

--- tasks/Export-Lists.ps1
+++ tasks/Export-Lists.ps1
@@ -1,7 +1,7 @@
 # Make sure to run Connect-PnPOnline first
-# Get-PnPProvisioningTemplate -Handlers "Lists" -ListsToExtract "TechCategories", "Tech" -Out ./tasks/schemas/Lists.TechStacker.xml
+Get-PnPProvisioningTemplate -Handlers "Lists" -ListsToExtract "TechCategories", "Tech" -Out ./tasks/schemas/Lists.TechStacker.xml
 
 
 Write-Host "Extracting Items"
-# Add-PnPDataRowsToProvisioningTemplate -path ./tasks/schemas/Lists.TechStacker.xml -List "TechCategories" -Query '<view></view>'
+Add-PnPDataRowsToProvisioningTemplate -path ./tasks/schemas/Lists.TechStacker.xml -List "TechCategories" -Query '<view></view>'
 Add-PnPDataRowsToProvisioningTemplate -path ./tasks/schemas/Lists.TechStacker.xml -List "Tech" -Query '<view></view>'
