eeb4b5504664de16ad3165ef377085abaa2575e0
Petersen <apetersen@skylinetechnologies.com>
2020-03-24 14:46:12 -0500

    updated useDebounce to useDebouncedEffect

--- src/ui-toolkit/hooks/useDebounce.ts
+++ src/ui-toolkit/hooks/useDebounce.ts
@@ -1,32 +1,39 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef, EffectCallback } from "react";
+// https://dev.to/droopytersen/usedebouncedeffect-hook-4204
+export function useDebouncedEffect<T>(
+  effectFn: (debouncedValue: T) => void | (() => void),
+  value: T,
+  delay: number
+) {
+  let effectRef = useRef(effectFn);
+  let updatedValue = useDebouncedValue(value, delay);
+  useEffect(() => {
+    if (effectRef.current) {
+      return effectRef.current(updatedValue);
+    }
+  }, [updatedValue]);
+}
 
-export default function useDebounce(value, delay) {
+export function useDebouncedValue(value, delay) {
   // State and setters for debounced value
   const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
-    // Set debouncedValue to value (passed in) after the specified delay
+    // Update state to the passed in value after the specified delay
     const handler = setTimeout(() => {
       setDebouncedValue(value);
     }, delay);
 
     return () => {
+      // If our value changes (or the component unmounts), React will
+      // run this cleanup function to cancel the state update.
       clearTimeout(handler);
     };
-  }, [value, delay]); // ... need to be able to change that dynamically. // You could also add the "delay" var to inputs array if you ... // Only re-call effect if value changes
+    // These are the dependencies, if the value or the delay amount
+    // changes, then cancel any existing timeout and start waiting again
+  }, [value, delay]);
 
   return debouncedValue;
 }
 
-// EXAMPLE usage
-// let updatedTitle = useDebounce(title, 200);
-// useEffect(() => {
-//     props.onChange(updatedTitle);
-// }, [updatedTitle])
-
-export function useDebouncedEffect(value, delay, effect) {
-  let updatedValue = useDebounce(value, delay);
-  useEffect(() => {
-    effect(updatedValue);
-  }, [updatedValue]);
-}
+export default useDebouncedValue;
